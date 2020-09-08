f8ad48d9907146d69262564dbe16dbbc3e771615
Mike McGrath <mike@mikemcgrathonline.us>
2020-09-08 16:34:15 -0400

    Container

---
+++ client/components/ui/layout/Container.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import MUIContainer from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  root: {},
+}))
+
+const Container = ({ fluid, fixed, children, ...rest }) => {
+  return (
+    <MUIContainer disableGutters={fluid} fixed={fixed} maxWidth="xl" classes={useStyles} {...rest}>
+      {children}
+    </MUIContainer>
+  )
+}
+
+Container.defaultProps = {
+  fluid: false,
+  fixed: false,
+}
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+  fluid: PropTypes.bool,
+  fixed: PropTypes.bool,
+}
+
+export default Container
--- client/containers/App.js
+++ client/containers/App.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './Home'
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/about">
-          <h1>About</h1>
-        </Route>
+    <Fragment>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
 
-        <Route exact path="/">
-          <h1>Home</h1>
-        </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <Route>
-          <h1>404</h1>
-        </Route>
-      </Switch>
-    </Router>
+          <Route>
+            <h1>404</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
   )
 }
 
