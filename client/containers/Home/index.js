0ef6233ad3520e8bbc1ac716f8cfd6445e685f4d
Mike McGrath <mike@mikemcgrathonline.us>
2020-09-17 19:34:00 -0400

    Hero

--- client/containers/Home/index.js
+++ client/containers/Home/index.js
@@ -6,15 +6,18 @@ import HomePage from '../../layouts/public/HomeLayout'
 const Home = props => {
   return (
     <HomePage>
-        <GridItem colWidth={6}>
-          <h1>Home Mike</h1>
-        </GridItem>
-        <GridItem colWidth={6}>
-          <h1>Home Mike</h1>
-        </GridItem>
-        <GridItem>
-          <h1>Home</h1>
-        </GridItem>
+      <HomePage.Hero>
+        <h1>Mike</h1>
+      </HomePage.Hero>
+      <GridItem colWidth={6}>
+        <h1>Home Mike</h1>
+      </GridItem>
+      <GridItem colWidth={6}>
+        <h1>Home Mike</h1>
+      </GridItem>
+      <GridItem>
+        <h1>Home</h1>
+      </GridItem>
     </HomePage>
   )
 }
